0d2f95a1e852cda565b3fde7d6b84146810a154d
Austin Bonander <austin@launchbadge.com>
2021-07-21 19:07:23 -0700

    WIP fix(sqlite): reset in worker to avoid race conditions

--- sqlx-core/src/sqlite/statement/worker.rs
+++ sqlx-core/src/sqlite/statement/worker.rs
@@ -6,6 +6,8 @@ use futures_channel::oneshot;
 use std::sync::{Arc, Weak};
 use std::thread;
 
+use libsqlite3_sys::{sqlite3_reset, sqlite3_step, SQLITE_DONE, SQLITE_OK, SQLITE_ROW};
+
 // Each SQLite connection has a dedicated thread.
 
 // TODO: Tweak this so that we can use a thread pool per pool of SQLite3 connections to reduce
@@ -21,6 +23,10 @@ enum StatementWorkerCommand {
         statement: Weak<StatementHandle>,
         tx: oneshot::Sender<Result<Either<u64, ()>, Error>>,
     },
+    Reset {
+        statement: Weak<StatementHandle>,
+        tx: oneshot::Sender<Result<(), Error>>,
+    },
 }
 
 impl StatementWorker {
@@ -31,13 +37,34 @@ impl StatementWorker {
             for cmd in rx {
                 match cmd {
                     StatementWorkerCommand::Step { statement, tx } => {
-                        let resp = if let Some(statement) = statement.upgrade() {
-                            statement.step()
+                        let statement = if let Some(statement) = statement.upgrade() {
+                            statement
                         } else {
-                            // Statement is already finalized.
-                            Err(Error::WorkerCrashed)
+                            // statement is already finalized, the sender shouldn't be expecting a response
+                            continue;
                         };
-                        let _ = tx.send(resp);
+
+                        // SAFETY: only the `StatementWorker` calls this function
+                        let status = unsafe { sqlite3_step(statement.as_ptr()) };
+                        let result = match status {
+                            SQLITE_ROW => Ok(Either::Right(())),
+                            SQLITE_DONE => Ok(Either::Left(statement.changes())),
+                            _ => Err(statement.last_error().into()),
+                        };
+
+                        let _ = tx.send(result);
+                    }
+                    StatementWorkerCommand::Reset { statement, tx } => {
+                        if let Some(statement) = statement.upgrade() {
+                            // SAFETY: this must be the only place we call `sqlite3_reset`
+                            let result = unsafe { sqlite3_reset(statement.as_ptr()) };
+
+                            let _ = tx.send(if result == SQLITE_OK {
+                                Ok(())
+                            } else {
+                                Err(statement.last_error().into())
+                            });
+                        }
                     }
                 }
             }
@@ -54,6 +81,20 @@ impl StatementWorker {
 
         self.tx
             .send(StatementWorkerCommand::Step {
+                statement: Arc::downgrade(statement),
+                tx,
+            })
+            .map_err(|_| Error::WorkerCrashed)?;
+
+        rx.await.map_err(|_| Error::WorkerCrashed)?
+    }
+
+    pub(crate) fn reset(&mut self, statement: &Arc<StatementHandle>) -> Result<(), Error> {
+        let (tx, rx) = oneshot::channel();
+
+        let _ = self
+            .tx
+            .send(StatementWorkerCommand::Reset {
                 statement: Arc::downgrade(statement),
                 tx,
             })
